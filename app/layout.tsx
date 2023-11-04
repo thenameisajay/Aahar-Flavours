e184fdc6d7dbffd476b52e9f917773606422f7aa
apm30 <m.ajaypradeep@icloud.com>
2023-11-04 22:57:49 +0000

    Refactor layout and add home page

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./global.css";
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="container mx-auto px-4">
+          <main className="py-4">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
