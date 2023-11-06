3dc780b3a6ceb0ab245f2d56e9efa43910f392c3
apm30 <m.ajaypradeep@icloud.com>
2023-11-06 20:15:06 +0000

    images rounded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <About />
       <Foods />
       <Testimonials />
+
       <Footer />
     </>
   );
--- src/app/ui/carousel.tsx
+++ src/app/ui/carousel.tsx
@@ -1,41 +1,39 @@
 import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import * as React from "react";
-import Stack from "@mui/joy/Stack";
-import Card from "@mui/joy/Card";
 
 export default function CarouselWithContent() {
   return (
-    <div className="mx-auto mt-2  rounded-lg w-8/12">
-      <Card size="lg">
-        <Carousel className="rounded-lg w-full">
-          <div>
-            <Image
-              src="/images/carousel/1.webp"
-              alt="ist Pic"
-              width={800}
-              height={800}
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/carousel/2.webp"
-              alt="2nd Pic"
-              width={800}
-              height={800}
-            />
-          </div>
-          <div>
-            <Image
-              src="/images/carousel/3.webp"
-              alt="3rd Pic"
-              width={800}
-              height={800}
-            />
-          </div>
-        </Carousel>
-      </Card>
+    <div className="mx-auto mt-2  w-11/12 h-4/5 rounded-lg  justify-center items-center">
+      <Carousel stopOnHover={false} autoPlay={true} infiniteLoop={true}>
+        <div>
+          <Image
+            className="rounded-2xl"
+            src="/images/carousel/1.webp"
+            alt="ist Pic"
+            width={800}
+            height={800}
+          />
+        </div>
+        <div>
+          <Image
+            className="rounded-2xl"
+            src="/images/carousel/2.webp"
+            alt="2nd Pic"
+            width={800}
+            height={800}
+          />
+        </div>
+        <div>
+          <Image
+            className="rounded-2xl"
+            src="/images/carousel/3.webp"
+            alt="3rd Pic"
+            width={800}
+            height={800}
+          />
+        </div>
+      </Carousel>
     </div>
   );
 }
