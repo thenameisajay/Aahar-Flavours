6ce90bbdc276f601868ae6fc2069a6e90a1ee950
apm30 <m.ajaypradeep@icloud.com>
2023-11-06 14:09:24 +0000

    minor typo fix

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -327,7 +327,7 @@ export default function Example() {
 
       <header className="relative bg-white">
         <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-          Get free delivery on orders over $100
+          Get free delivery on orders over £30
         </p>
 
         <nav
