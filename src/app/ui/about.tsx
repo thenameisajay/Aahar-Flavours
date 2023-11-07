6adc086350963d05d46e958110758b85a62975d3
apm30 <m.ajaypradeep@icloud.com>
2023-11-07 22:12:17 +0000

    Refactor about page UI layout

--- src/app/ui/about.tsx
+++ src/app/ui/about.tsx
@@ -6,25 +6,25 @@ export default function About() {
     <div className="bg-white">
       <div className="container mx-auto mt-12 dark:bg-white bg-white">
         <div className="card card-side  shadow-xl dark:bg-white bg-white">
-          <div className="flex flex-row">
-            <figure className="p-6">
+          <div className="flex flex-col sm:flex-row">
+            <div className="card-body">
               <Image
                 src="/images/about/chef.webp"
                 alt="Movie"
                 width={800}
                 height={800}
-                className="rounded-lg  max-[639px]:hidden"
+                className="rounded-lg"
                 priority
               />
-            </figure>
+            </div>
             <div className="card-body">
               <h2 className="card-title text-3xl">READ OUR STORY</h2>
               <h2 className="card-title font-sans text-gray-500 font-bold">
                 We&apos;ve been Making The Delicious Foods Since 2017
               </h2>
-              <p className="font-sans text-lg ">
+              <p className="font-sans text-lg">
                 Aahar was formed with a simple goal in mind, provide genuinely
-                fresh and authentic nepalese food to our customers We focus on
+                fresh and authentic Nepalese food to our customers. We focus on
                 using fresh, seasonal ingredients to create dishes that are true
                 to the traditional flavors and cooking methods of Nepalese and
                 North Indian cuisine. We try our best in creating dishes that
@@ -32,7 +32,7 @@ export default function About() {
                 Nepal, using authentic ingredients and cooking techniques. By
                 using fresh ingredients and staying true to traditional cooking
                 methods, we aim to provide diners with a unique and flavorful
-                dining experience that is a true reflection of the nepalese and
+                dining experience that is a true reflection of the Nepalese and
                 Indian culture and cuisine.
               </p>
             </div>
