d3c5d947d7985e3c9f06c5921a015af69faf158e
apm30 <m.ajaypradeep@icloud.com>
2023-11-12 18:13:16 +0000

    Remove unused imports in Cart component

--- src/app/ui/components/Cart.tsx
+++ src/app/ui/components/Cart.tsx
@@ -8,8 +8,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { use } from "react";
-import { clear } from "console";
 
 export default function Cart(props: any) {
   let cart = props.cart;
