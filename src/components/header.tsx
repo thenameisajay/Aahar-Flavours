185150e205b87a492900734848528f12c3813d39
apm30 <m.ajaypradeep@icloud.com>
2024-03-29 17:06:33 +0000

    total ui changeover optimisation under the hood

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,58 +1,11 @@
-'use client';
-
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { Phone } from 'lucide-react';
 
 import MobileMenu from '@/components/Mobile-menu';
 import CartComponent from '@/components/cart/Cart-component';
-
-export default function Header() {
-    return (
-        <div className="bg-white">
-            <header className="relative bg-white">
-                <p className="flex h-10 items-center justify-center bg-green-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-                    Get free delivery on orders over £30
-                </p>
-
-                <nav
-                    aria-label="Top"
-                    className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
-                >
-                    <div className="border-b border-gray-200">
-                        <div className="flex h-16 items-center text-center">
-                            {/* Logo */}
-                            <MobileMenu />
-                            <div className="ml-4 flex text-center flex-row lg:ml-0">
-                                <a href="#">
-                                    <span className="sr-only">
-                                        Aahar Flavours
-                                    </span>
-                                    <Image
-                                        src="/images/logo/logo-removebg-preview.png"
-                                        alt="Aahar Flavours Logo"
-                                        width={35}
-                                        height={35}
-                                        className="mr-1"
-                                    />
-                                </a>
-                                <h2 className="   justify-center  text-base p-1 text-green-500  font-sans italic font-extrabold">
-                                    Aahar Flavours
-                                </h2>
-                            </div>
-                            {/* Section buttons here */}
-
-                            <div className="ml-auto flex items-center">
-                                <MoreDetailsComponent />
-                                <CartComponent />
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
-        </div>
-    );
-}
+import { mainNav } from '@/config/docs';
 
 const MoreDetailsComponent = () => {
     return (
@@ -92,3 +45,86 @@ const MoreDetailsComponent = () => {
         </>
     );
 };
+
+const IntroImageComponent = () => {
+    return (
+        <div className="ml-4 flex text-center flex-row lg:ml-0">
+            <a href="#">
+                <span className="sr-only">Aahar Flavours</span>
+                <Image
+                    src="/images/logo/logo-removebg-preview.png"
+                    alt="Aahar Flavours Logo"
+                    width={35}
+                    height={35}
+                    className="mr-1"
+                />
+            </a>
+            <h2 className="   justify-center  text-base p-1 text-green-500  font-sans italic font-extrabold">
+                Aahar Flavours
+            </h2>
+        </div>
+    );
+};
+
+const SectionButtonsComponent = () => {
+    return (
+        <div className="ml-auto flex items-center">
+            <MoreDetailsComponent />
+            <CartComponent />
+        </div>
+    );
+};
+
+const LargeMenuComponent = () => {
+    return (
+        <div className=" px-2 relative   flex mx-2 ">
+            {mainNav.map(
+                (item) =>
+                    item.href && (
+                        <div key={item.title}>
+                            <Link
+                                href={item.href}
+                                className={`text-sm mx-2  font-medium text-muted-foreground transition-colors hover:text-primary  hover:bg-slate-100  dark:hover:bg-slate-800 p-2 rounded-full `}
+                            >
+                                {item.title}
+                            </Link>
+                        </div>
+                    ),
+            )}
+        </div>
+    );
+};
+
+const NavBarComponent = () => {
+    return (
+        <nav
+            aria-label="Top"
+            className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+        >
+            <div className="border-b border-gray-200">
+                <div className="flex h-16 items-center text-center ">
+                    <MobileMenu />
+                    <IntroImageComponent />
+                    <div className="hidden lg:block ">
+                        <LargeMenuComponent />
+                    </div>
+
+                    <SectionButtonsComponent />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default function Header() {
+    return (
+        <div className="bg-white">
+            <header className="relative bg-white">
+                <p className="flex h-10 items-center justify-center bg-green-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+                    Get free delivery on orders over £30
+                </p>
+                <NavBarComponent />
+            </header>
+        </div>
+    );
+}
