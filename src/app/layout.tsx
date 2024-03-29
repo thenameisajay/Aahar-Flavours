35323ab7ba162438db0de9dfd784089ed4f260be
apm30 <m.ajaypradeep@icloud.com>
2024-03-29 17:17:51 +0000

    making site info global to access anywhere

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,21 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { siteConfig } from '@/config/site';
+
 import './globals.css';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Aahar Flavours - Fresh Authentic Cuisine',
-    applicationName: 'Aahar Flavours',
-    authors: [{ name: 'Aahar Flavours' }],
-    description:
-        'Nepalese fresh authentic food, Flavours, Best food, Indian, North Indian',
-    keywords:
-        'Nepalese, Authentic, Flavours, Reading, Twyford, Wokingham, authentic food, flavours, best food, indian, north indian, comfort food, hungry, spices, herbs, curry, naan, wings, chicken, dal makhni',
-    icons: {
-        icon: { url: '/images/logo/logo.png' },
-        shortcut: ['/images/logo/logo.png'],
-        apple: '/favicons/apple-touch-icon.png',
-        other: [
-            {
-                rel: '/favicons/android-chrome-384x384.png',
-                url: '/favicons/android-chrome-384x384.png',
-            },
-        ],
-    },
+    title: siteConfig.title,
+    applicationName: siteConfig.applicationName,
+    authors: siteConfig.authors,
+    description: siteConfig.description,
+    keywords: siteConfig.keywords,
+    icons: siteConfig.icons,
+    creator: 'thenameisajay - https://github.com/thenameisajay',
 };
 
 export default function RootLayout({
